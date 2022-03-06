9899105b191c057dcad7c3d237cd98df9cbbe978
B0922044 <B0922044@cgu.edu.tw>
2022-03-06 22:03:47 +0800

    HW#1_lotto

--- hw1/main.c
+++ hw1/main.c
@@ -5,13 +5,13 @@
 int main(void) {
 	FILE* fp;
 	int n;
-	int num[5][7];
+	int num[6];
 		
 	printf("歡迎光臨長庚樂透彩購買機台\n請問您要買幾組樂透彩 : ");
 	scanf("%d", &n);
 
 	fp = fopen("lotto.txt", "w+");
-//output time
+//time
 	time_t curtime;
 	struct tm *info;
 	char buffer[80];
@@ -20,28 +20,35 @@ int main(void) {
 	fprintf(fp, "========= lotto649 =========\n");
 	strftime(buffer, 80, "%a %b %d %X %Y", info);
 	fprintf(fp, "= %s =\n", buffer);
-//output random number
-	for(int i = 0; i < 5; i++) {
-		fprintf(fp, "[%d]: ", i + 1);
-		num[i][6] = rand() % 10 + 1;
-		for(int j = 0; j < 6; j++) {
-			num[i][j] = rand() % 69 + 1;
-			if(i >= n) {
+//random number
+	for(int i = 1; i <= 5; i++) {
+		fprintf(fp, "[%d]: ", i);
+		num[5] = rand() % 10 + 1;
+		for(int j = 0; j < 5; j++) {
+			num[j] = rand() % 69 + 1;
+			if(i > n) {
 				fprintf(fp, "-- ");
-			} else if(num[i][j] < 10) {
-				fprintf(fp, "0%d ", num[i][j]);
+			} else if(num[j] < 10) {
+				fprintf(fp, "0%d ", num[j]);
 			} else {
-				fprintf(fp, "%d ", num[i][j]);
+				fprintf(fp, "%d ", num[j]);
 			}
-			if(num[i][6] == num[i][j]) {
-				num[i][6] = rand() % 10 + 1;
+			if(num[5] == num[j]) {
+				num[5] = rand() % 10 + 1;
 			}
 		}
-		fprintf(fp, "\n");
+//special number
+		if(i > n) {
+			fprintf(fp, "--\n");
+		} else if(num[5] == 10) {
+			fprintf(fp, "%d\n", num[5]);
+		} else {
+			fprintf(fp, "0%d\n", num[5]);
+		}
 	}
-
-	fprintf(fp, "========= csie@CGU =========\n");
+	fprintf(fp, "========= csie@CGU =========");
 	fclose(fp);
+
 	printf("已為您購買的 %d 組樂透彩組合輸出至 lotto.txt", n);
 
 	return 0;
