ca2bf990f5a3ca7388ddd8d75077416293302fd9
B0922044 <B0922044@cgu.edu.tw>
2022-03-06 16:17:55 +0800

    HW#1_lotto

--- hw1/main.c
+++ hw1/main.c
@@ -6,25 +6,27 @@ int main(void) {
 	FILE* fp;
 	int n;
 	int num[5][7];
-	
+		
 	printf("歡迎光臨長庚樂透彩購買機台\n請問您要買幾組樂透彩 : ");
 	scanf("%d", &n);
 	printf("已為您購買的 %d 組樂透彩組合輸出至 lotto.txt", n);
 
+	fp = fopen("lotto.txt", "w+");
+//output time
 	time_t curtime;
 	struct tm *info;
 	char buffer[80];
 	time(&curtime);
 	info = localtime(&curtime);
-
-	fp = fopen("lotto.txt", "wb+");
 	fprintf(fp, "========= lotto649 =========\n");
-	strftime(buffer,80,"%a %b %d %X %Y", info);
+	strftime(buffer, 80, "%a %b %d %X %Y", info);
 	fprintf(fp, "= %s =\n", buffer);
+
+//output random number
 	for(int i = 0; i < 5; i++) {
 		fprintf(fp, "[%d]: ", i + 1);
 		for(int j = 0; j < 7; j++) {
-			num[i][j] = rand() % 70;
+			num[i][j] = rand() % 69 + 1;
 			if(i >= n) {
 				fprintf(fp, "-- ");
 			} else if(num[i][j] < 10) {
@@ -35,6 +37,7 @@ int main(void) {
 		}
 		fprintf(fp, "\n");
 	}
+
 	fprintf(fp, "========= csie@CGU =========\n");
 	fclose(fp);
 
