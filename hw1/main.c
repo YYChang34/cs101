5bc10ecbdfdb9914c741dc85338f6b1511810537
B0922044 <B0922044@cgu.edu.tw>
2022-03-06 21:31:58 +0800

    HW#1_lotto

--- hw1/main.c
+++ hw1/main.c
@@ -9,7 +9,6 @@ int main(void) {
 		
 	printf("歡迎光臨長庚樂透彩購買機台\n請問您要買幾組樂透彩 : ");
 	scanf("%d", &n);
-	printf("已為您購買的 %d 組樂透彩組合輸出至 lotto.txt", n);
 
 	fp = fopen("lotto.txt", "w+");
 //output time
@@ -21,11 +20,11 @@ int main(void) {
 	fprintf(fp, "========= lotto649 =========\n");
 	strftime(buffer, 80, "%a %b %d %X %Y", info);
 	fprintf(fp, "= %s =\n", buffer);
-
 //output random number
 	for(int i = 0; i < 5; i++) {
 		fprintf(fp, "[%d]: ", i + 1);
-		for(int j = 0; j < 7; j++) {
+		num[i][6] = rand() % 10 + 1;
+		for(int j = 0; j < 6; j++) {
 			num[i][j] = rand() % 69 + 1;
 			if(i >= n) {
 				fprintf(fp, "-- ");
@@ -34,12 +33,16 @@ int main(void) {
 			} else {
 				fprintf(fp, "%d ", num[i][j]);
 			}
+			if(num[i][6] == num[i][j]) {
+				num[i][6] = rand() % 10 + 1;
+			}
 		}
 		fprintf(fp, "\n");
 	}
 
 	fprintf(fp, "========= csie@CGU =========\n");
 	fclose(fp);
+	printf("已為您購買的 %d 組樂透彩組合輸出至 lotto.txt", n);
 
 	return 0;
 }
