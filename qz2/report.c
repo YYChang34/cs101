c3c4b275dd116c55e0f83c0a4873c80c3649fc29
B0922044 <B0922044@cgu.edu.tw>
2022-03-22 22:51:54 +0800

    QZ#2

--- qz2/report.c
+++ qz2/report.c
@@ -20,8 +20,6 @@ int main(void) {
 	FILE* fp_record_w = fopen("report.txt", "w+");
 	fprintf(fp_record_w, "========= lotto649 Report =========\n");
 	fprintf(fp_record_w, "- Date ------ Num. ------ Receipt -\n");
-	lotto_record_t lotto_record[50];
-	only_record_t record[50];
 	
 	char line_ch;
 	int count = 0;
@@ -31,6 +29,8 @@ int main(void) {
 	int total_case = count / sizeof(lotto_record_t);
 	fclose(fp_record_r);
 
+	lotto_record_t lotto_record[total_case];
+	only_record_t record[total_case];
 	fp_record_r = fopen("records.bin", "r");
 	fread(lotto_record, sizeof(lotto_record_t), total_case, fp_record_r);
 	for(int i = 0; i < total_case; i++) {
@@ -40,8 +40,8 @@ int main(void) {
 	fclose(fp_record_r);
 
 	int k, m, n;
-	int count_case[50], count_group[50], case_receipt[50];
-	char case_date[50][9];
+	int count_case[total_case], count_group[total_case], case_receipt[total_case];
+	char case_date[total_case][9];
 	for(int j = 0; j < total_case; j++) {
 		count_case[j] = 1;
 		case_receipt[j] = 0;
@@ -82,10 +82,10 @@ int main(void) {
 
 	time_t curtime;
 	struct tm *info;
-	char buffer[80];
+	char buffer[9];
 	time(&curtime);
 	info = localtime(&curtime);
-	strftime(buffer, 80, "%Y%m%d", info);
+	strftime(buffer, 9, "%Y%m%d", info);
 	fprintf(fp_record_w, "======== %s Printed =========\n", buffer);
 	fclose(fp_record_w);
 	
