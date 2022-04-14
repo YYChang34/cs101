b5c67b3be271f83b6389227561a6676f3be0efea
B0922044 <B0922044@cgu.edu.tw>
2022-04-14 18:20:58 +0800

    LAB#4

--- lab4/main.c
+++ lab4/main.c
@@ -8,22 +8,15 @@ void find_num(int* p, int num) {
 		}
 	}
 }
-void sort(int** ap) {
-	int i, j;
+void sort(int** ap, int n[10]) {
+	int i, j, a, b;
 	for(i = 0; i < 10; i++) {
-		int a = i;
-		for(j = i + 1; j < 10; j++) {
-			if(*ap[i] > *ap[j]) {
-				int* temp = ap[i];
-				ap[i] = ap[j];
-				ap[j] = temp;
-				int temp_num = a;
-				a = j;
-				j = temp_num;
+		for(j = 0; j < 10; j++) {
+			if(*ap[i] == n[j]) {
+				printf("&n[%d]->%p, n[%d] = %d; ", j, ap[i], j, n[j]);
+				printf("ap[%d]->%p, *ap[%d] = %d\n", i, ap[i], i, *(ap[i]));
 			}
 		}
-		printf("&n[%d]->%p, n[%d] = %d, ", a, ap[a], a, *(ap[i]));
-		printf("ap[%d]->%p, *ap[%d] = %d\n", i, ap[i], i, *(ap[i]));
 	}
 }
 
@@ -33,13 +26,23 @@ int main(void) {
 	int* ap[10];
 	int num = 9;
 	p = n;
-	for(int i = 0, j = 0; i < 10; i++) {
+	int i, j;
+	for(i = 0, j = 0; i < 10; i++) {
 		ap[i] = &n[j++];
+	}
+	for(i = 0; i < 10; i++) {
+		for(j = i + 1; j < 10; j++) {
+			if(*ap[i] > *ap[j]) {
+				int* temp = ap[i];
+				ap[i] = ap[j];
+				ap[j] = temp;
+			}
+		}
 	}
 	printf("N0.1 -------------------\n");
 	find_num(p, num);
 	printf("N0.2 -------------------\n");
-	sort(ap);
+	sort(ap, n);
 
 	return 0;
 }
