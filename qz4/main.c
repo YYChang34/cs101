c87ef0ffc6abebaad86f374e8692d99cdbc93799
B0922044 <B0922044@cgu.edu.tw>
2022-04-19 14:49:37 +0800

    QZ#4

--- qz4/main.c
+++ qz4/main.c
@@ -37,12 +37,14 @@ char* copy_string(char* s) {
 }
 
 int main(void) {
+	printf("No.1 -------------------\n");
 	int n, m;
 	n = 1;
 	m = 2;
 	swap(&n, &m);
 	printf("after swap, n=%d, m=%d\n", n, m);
 
+	printf("No.2 -------------------\n");
 	int size = 10;
 	int sourse[10] = {0, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 	int dest[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
@@ -52,6 +54,7 @@ int main(void) {
 	printf("after swap array, dest array = [");
 	printArray(dest, size);
 
+	printf("No.3 -------------------\n");
 	char str[] = "IU!IU!IU!IU!";
 	char* cp_str = copy_string(str);
 	printf("copy string = %s\n", cp_str);
