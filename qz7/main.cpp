b4dc37ebe8e03282febc260ab4b20236d55d3e95
B0922044 <B0922044@cgu.edu.tw>
2022-06-14 14:57:25 +0800

    QZ#7

---
+++ qz7/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class myString {};
+
+class ReadClass {
+	public:
+		ifstream in;
+		string str;
+		string str_last;
+		int num_of_class;
+
+	void showClass() {
+		in.open("main.cpp");
+		num_of_class = 0;
+		while(getline(in, str)) {
+			int str_start = str.find("class", 0);
+			int class_name = str.find("{", str_start);
+			if(str_start == 0 && class_name != -1) {
+				num_of_class++;
+				for(int i = str_start; i < class_name - 1; i++) {
+					cout << str[i];
+				}
+				cout << endl;
+			}
+		}
+		in.close();
+		cout << num_of_class << " class in main.cpp" << endl;
+	}
+};
+
+int main(void) {
+	ReadClass rfile;
+	rfile.showClass();
+	return 0;
+}
